0b541f788f4c15841371f087747f4bf5afc3e16f
Alexandre Berrada <Alexandre.Berrada@grenoble-inp.org>
2018-11-22 09:40:20 +0100

    Début fonction DemandeEnchere dans le cas montante et enchère simple.

--- JDBC/jdbc/DemandeEnchere.java
+++ JDBC/jdbc/DemandeEnchere.java
@@ -9,7 +9,7 @@ public class DemandeEnchere{
     private String emailAcheteur;
 
 
-    public DemandeEnchere(Connection con, String emailAcheteur, int idVente){
+    public DemandeEnchere(Connection con, String emailAcheteur, int idVente, boolean enchereMontante, boolean enchereMultiple){
 
         /*
          * Cette fonction effectue une demande d'enchère sur une vente.
@@ -23,56 +23,73 @@ public class DemandeEnchere{
          * Ce processus est réalisé par les 2 boucles while suivantes.
          */
 
-        boolean nombreCorrect = False;
-        boolean enchereSuffisante = False;
+        // Si l'enchère est montante :
+        if (enchereMontante) {
 
-        while (!enchereSuffisante){
-            while (!nombreCorrect) {
+            // Si les enchères multiples ne sont pas autorisées, il faut s'assurer que le client a le droit d'enchérir :
 
-                // L'utilisateur saisit un nombre et on vérifie qu'il est correct :
-                System.out.println("Veuillez saisir votre offre pour ce produit : ");
-                String offre = sc.nextLine();
-                nombreCorrect = True;
-                try{
-                    int prixOffre = Integer.parseInt(offre);
-                } catch (Exception e) {
-                    System.out.println("ERREUR : Vous avez saisi un nombre incorrect!");
-                    nombreCorrect = False;
+            if (!enchereMultiple) {
+                PreparedStatement droitEnchere = con.prepareStatement("SELECT COUNT(PRIX_ENCHERE) FROM ENCHERES WHERE emailAcheteur =? ");
+                droitEnchere.setString(1, emailAcheteur);
+                ResultSet nombreEnchere = droitEnchere.executeQuery();
+
+                if () {
+
+                }
+
+            }
+
+            boolean nombreCorrect = False;
+            boolean enchereSuffisante = False;
+
+            while (!enchereSuffisante){
+                while (!nombreCorrect) {
+
+                    // L'utilisateur saisit un nombre et on vérifie qu'il est correct :
+                    System.out.println("Veuillez saisir votre offre pour ce produit : ");
+                    String offre = sc.nextLine();
+                    nombreCorrect = True;
+                    try{
+                        int prixOffre = Integer.parseInt(offre);
+                    } catch (Exception e) {
+                        System.out.println("ERREUR : Vous avez saisi un nombre incorrect!");
+                        nombreCorrect = False;
+                    }
+                }
+                // On vérifie maintenant que l'offre est suffisante :
+
+                PreparedStatement prixEncheres = con.prepareStatement("SELECT MAX(PRIX_ENCHERE) FROM ENCHERES WHERE ID_VENTE =? ");
+                prixEncheres.setInt(1, idVente);
+                ResultSet rs = prixEncheres.executeQuery();
+                // Comparaison des nombres :
+                int prixActuel = rs.getInt("PRIX_ENCHERE");
+                enchereSuffisante = (prixOffre > prixActuel);
+
+                // On informe le client si l'offre est insuffisante :
+                if (!enchereSuffisante) {
+                    System.out.println("Votre offre est insuffisante. Le prix actuel de l'enchère est :");
+                    System.out.println(prixActuel);
+                    System.out.println("Vous devez proposer plus si vous voulez acquérir cet article.");
                 }
             }
-            // On vérifie maintenant que l'offre est suffisante :
 
-            PreparedStatement prixEncheres = con.prepareStatement("SELECT MAX(PRIX_ENCHERE) FROM ENCHERES WHERE ID_VENTE =? ");
-            prixEncheres.setInt(1, idVente);
-            ResultSet rs = prixEncheres.executeQuery();
-            // Comparaison des nombres :
-            int prixActuel = rs.getInt("PRIX_ENCHERE");
-            enchereSuffisante = (prixOffre > prixActuel);
+            // TODO : Insérer l'offre dans la table:
 
-            // On informe le client si l'offre est insuffisante :
-            if (!enchereSuffisante) {
-                System.out.println("Votre offre est insuffisante. Le prix actuel de l'enchère est :");
-                System.out.println(prixActuel);
-                System.out.println("Vous devez proposer plus si vous voulez acquérir cet article.");
-            }
+            // On cherche une nouvelle clé primaire :
+            PreparedStatement clesPrimairesExistantes = con.prepareStatement("SELECT MAX(ID_ENCHERE) FROM ENCHERES");
+            ResultSet cleMax = clesPrimairesExistantes.executeQuery();
+            int clePrimaire = cleMax.getInt("id_enchere") + 1;
+
+            // On insère l'offre :
+            PreparedStatement enchere = con.prepareStatement("INSERT INTO ENCHERES VALUES (=?, =?) ");
+            enchere.setInt(1, clePrimaire + 1);
+            enchere.setInt(2, prixOffre);
+            enchere.setString(3, emailUtilisateur);
+            enchere.setInt(4, idVente);
+
+            ResultSet rs = salle.executeQuery();
+            PreparedStatement commit = con.prepareStatement("COMMIT");
+            commit.executeQuery();
         }
-
-        // TODO : Insérer l'offre dans la table:
-
-        // On cherche une nouvelle clé primaire :
-        PreparedStatement clesPrimairesExistantes = con.prepareStatement("SELECT MAX(ID_ENCHERE) FROM ENCHERES");
-        ResultSet cleMax = clesPrimairesExistantes.executeQuery();
-        int clePrimaire = cleMax.getInt("id_enchere") + 1;
-
-        // On insère l'offre :
-        PreparedStatement enchere = con.prepareStatement("INSERT INTO ENCHERES VALUES (=?, =?) ");
-        enchere.setInt(1, clePrimaire + 1);
-        enchere.setInt(2, prixOffre);
-        enchere.setString(3, emailUtilisateur);
-        enchere.setInt(4, idVente);
-
-        ResultSet rs = salle.executeQuery();
-        PreparedStatement commit = con.prepareStatement("COMMIT");
-        commit.executeQuery();
     }
 }
