447120f334f670a1b65c3531ac41c5c712d8edad
Aurélien Pora <aurelien.pora@grenoble-inp.org>
2018-12-07 17:59:25 +0100

    nouvelle Vente fonctionne

--- JDBC/jdbc/NouvelleVente.java
+++ JDBC/jdbc/NouvelleVente.java
@@ -49,10 +49,15 @@ public class NouvelleVente{
       Type salleType = new Type(con, categorie, estUnique, estDescendante, estRevocable, estLimitee);
       int idSalle = salleType.getId();
       if (idSalle == -1) {
-        PreparedStatement nouvelleSalle = con.prepareStatement("INSERT INTO SALLE VALUE (?, ?)");
+        Statement salleMax = con.createStatement();
+        getMax = salleMax.executeQuery("SELECT MAX(id_salle) FROM SALLE ");
+        getMax.next();
+        max = getMax.getInt("MAX(id_salle)") + 1;
+        PreparedStatement nouvelleSalle = con.prepareStatement("INSERT INTO SALLE VALUES (?, ?)");
         nouvelleSalle.setInt(1, max);
         nouvelleSalle.setString(2, categorie);
-        idSalle = max + 1;
+        nouvelleSalle.executeUpdate();
+        idSalle = max;
       }
       System.out.println("Nous allons vous demander les champs relatifs au produit");
       System.out.println("Entrez le nom du produit");
@@ -71,15 +76,16 @@ public class NouvelleVente{
       Statement prodMax = con.createStatement();
       getMax = prodMax.executeQuery("SELECT MAX(id_produit) FROM PRODUIT ");
       getMax.next();
-      int idProduit = getMax.getInt("MAX(id_produit)");
-      produit.setInt(1, max);
+      int idProduit = getMax.getInt("MAX(id_produit)")+1;
+      produit.setInt(1, idProduit);
       produit.setString(2, nomProduit);
       produit.setInt(3, prixRevient);
       produit.setInt(4, stock);
       produit.setString(5, categorie);
       produit.setInt(6, idSalle);
       System.out.println(idProduit);
-      // produit.executeUpdate();
+      System.out.println(idSalle);
+      produit.executeUpdate();
 
       System.out.println("Nous allons maintenant determiner les informations relatives a la vente");
       System.out.println("Entrez le prix de depart");
@@ -90,70 +96,73 @@ public class NouvelleVente{
       Statement venteMax = con.createStatement();
       getMax = venteMax.executeQuery("SELECT MAX(id_produit) FROM PRODUIT ");
       getMax.next();
-      int idVente = getMax.getInt("MAX(id_produit)");
-      PreparedStatement vente = con.prepareStatement("INSERT INTO VENTE (id_vente, prix_depart_vente, id_salle, unicite_enchere, sens_vente, annulation_vente, duree_vente, date_fin) VALUES (?, ?, ?, ?, ?, ?, ?, NULL)");
-      vente.setInt(1, idVente);
-      vente.setInt(2, prixDepart);
-      vente.setInt(3, idSalle);
-      if (estUnique) {
-        vente.setString(4, "'unique'");
-      }
-      else{
-        vente.setString(4, "'plusieurs'");
-      }
+      int idVente = getMax.getInt("MAX(id_produit)") + 1;
+      Statement vente = con.createStatement();
+      // PreparedStatement vente = con.prepareStatement("INSERT INTO VENTE (id_vente, prix_depart_vente, id_salle) VALUES (?, ?, ?)");
+      // vente.setInt(1, idVente);
+      // vente.setInt(2, prixDepart);
+      // vente.setInt(3, idSalle);
+      // if (estUnique) {
+      //   vente.setString(4, "'unique'");
+      // }
+      // else{
+      //   vente.setString(4, "plusieurs");
+      // }
+      //
+      // if (estDescendante) {
+      //   vente.setString(5, "descendante");
+      // }
+      // else{
+      //   vente.setString(5, "montante");
+      // }
+      //
+      // if (estRevocable) {
+      //   vente.setString(6, "revocable");
+      // }
+      // else{
+      //   vente.setString(6, "non_revocable");
+      // }
+      //
+      // if (estLimitee) {
+      //   System.out.println("C est limite");
+      //   vente.setString(7, "limitee");
+      // }
+      // else{
+      //   System.out.println("C est pas limite");
+      //   vente.setString(7, "non_limitee");
+      // }
 
-      if (estDescendante) {
-        vente.setString(5, "'descendante'");
-      }
-      else{
-        vente.setString(5, "'montante'");
-      }
-
-      if (estRevocable) {
-        vente.setString(6, "'revocable'");
-      }
-      else{
-        vente.setString(6, "'non_revocable'");
-      }
-
-      if (estLimitee) {
-        System.out.println("C est limite");
-        vente.setString(7, "'limitee'");
-      }
-      else{
-        System.out.println("C est pas limite");
-        vente.setString(7, "'non_limitee'");
-      }
-
-      // System.out.println(idSalle);
-      // vente.executeUpdate();
+      System.out.println(idSalle);
+      System.out.println(idVente);
+      System.out.println(prixDepart);
+      vente.executeQuery("INSERT INTO VENTE (id_vente, prix_depart_vente, id_salle) VALUES ("+idVente+", "+prixDepart+", "+idSalle+")");
 
       PreparedStatement prodSoumisVente = con.prepareStatement("INSERT INTO PRODUIT_SOUMIS_A_LA_VENTE VALUES(?, ?, ?)");
       prodSoumisVente.setInt(1, idProduit);
       prodSoumisVente.setInt(2, idVente);
       prodSoumisVente.setString(3, email);
-      // prodSoumisVente.executeUpdate();
+      prodSoumisVente.executeUpdate();
 
-      PreparedStatement salleVenteCree = con.prepareStatement("INSERT INT SALLE_DE_VENTE_CREEE VALUES (?, ?, ?)");
+      PreparedStatement salleVenteCree = con.prepareStatement("INSERT INTO SALLE_DE_VENTE_CREEE VALUES (?, ?, ?)");
       salleVenteCree.setInt(1, idSalle);
       salleVenteCree.setInt(2, idProduit);
       salleVenteCree.setInt(3, idVente);
-      // salleVenteCree.executeUpdate();
+      salleVenteCree.executeUpdate();
 
-      // if (estDescendante) {
-      //     PreparedStatement desc = con.prepareStatement("UPDATE VENTE SET sens_vente='descendante'");
-      //     desc.executeQuery();
-      // }
-      //
-      // if (estUnique){
-      //   PreparedStatement uniq = con.prepareStatement("UPDATE VENTE SET unicite_enchere='unique'");
-      //   uniq.executeQuery();
-      // }
-      //
+      if (estDescendante) {
+          PreparedStatement desc = con.prepareStatement("UPDATE VENTE SET sens_vente='descendante' WHERE id_vente=" + idVente);
+          desc.executeQuery();
+      }
+
+      if (estUnique){
+        PreparedStatement uniq = con.prepareStatement("UPDATE VENTE SET unicite_enchere='unique' WHERE id_vente=" + idVente);
+        uniq.executeQuery();
+      }
+
       if(estLimitee){
         // #TODO
 
-        PreparedStatement dur = con.prepareStatement("UPDATE VENTE SET date_fin = ? where");
+        PreparedStatement dur = con.prepareStatement("UPDATE VENTE SET date_fin = ? where id_vente=" + idVente);
         // UPDATE VENTE SET date_fin =timestamp '2018-11-26 10:20:00' where id_vente=5;
 
         System.out.println("Entrez l'annee de fin sous la forme aaaa mm jj hh mm");
@@ -162,7 +171,7 @@ public class NouvelleVente{
         int jourFin = sc.nextInt();
         int heureFin = sc.nextInt();
         int minuteFin = sc.nextInt();
-        System.out.println(anneeFin  +""+ moisFin +""+ jourFin +""+ heureFin +""+ minuteFin);
+        // System.out.println(anneeFin  +""+ moisFin +""+ jourFin +""+ heureFin +""+ minuteFin);
         dur.setTimestamp(1, new Timestamp(anneeFin, moisFin, jourFin, heureFin, minuteFin, 0, 0));
         dur.executeUpdate();
 
@@ -173,11 +182,11 @@ public class NouvelleVente{
 
       }
 
-      // if (estRevocable) {
-      //   PreparedStatement rev = con.prepareStatement("UPDATE VENTE SET annulation_vente='revocable'");
-      //   rev.executeQuery();
-      // }
-      //
+      if (estRevocable) {
+        PreparedStatement rev = con.prepareStatement("UPDATE VENTE SET annulation_vente='revocable' WHERE id_vente=" + idVente);
+        rev.executeQuery();
+      }
+
       PreparedStatement commit = con.prepareStatement("COMMIT");
       commit.executeQuery();
 
