56ae9b2a293d7ccf20f3acd31871e4980509ffca
Antoine Cheymol <antoine.cheymol@grenoble-inp.org>
2018-12-08 13:49:28 +0100

    updated

--- new_tables.sql
+++ new_tables.sql
@@ -21,38 +21,39 @@ insert into CARACTERISTIQUE values ('Région', 'Bourgogne', 3);
 insert into CARACTERISTIQUE values ('Région', 'Rhône-Alpes', 4);
 insert into CARACTERISTIQUE values ('Région', 'Champagne-Ardennes', 5);
 
-insert into ENCHERE values (1, 60, timestamp '2018-11-26 10:20:00', 5);
-insert into ENCHERE values (2, 65, timestamp '2018-11-26 08:00:00', 5);
-insert into ENCHERE values (3, 1500000, timestamp '2018-11-26 10:20:00', 1);
-insert into ENCHERE values (4, 1500000, timestamp '2018-11-26 08:00:00', 1);
-insert into ENCHERE values (5, 55, timestamp '2018-11-26 06:00:00', 5);
-
 insert into VENTE (id_vente, prix_depart_vente, id_salle) values (1, 50, 2);
 insert into VENTE (id_vente, prix_depart_vente, id_salle, unicite_enchere) values (2, 60, 3, 'unique');
-insert into VENTE (id_vente, prix_depart_vente, id_salle, sens_vente) values (3, 1000000, 1);
-insert into VENTE (id_vente, prix_depart_vente, id_salle, annulation_vente) values (4, 1000000, 1);
-insert into VENTE (id_vente, prix_depart_vente, id_salle, duree_vente, date_fin) values (5, 50, 2, 'limitee', timestamp '2018-11-26 10:20:00');
+insert into VENTE (id_vente, prix_depart_vente, id_salle, sens_vente) values (3, 1000000, 1, 'descendante');
+-- Ces 2 requêtes ne devraient pas fonctionner dans java : 1 SEUL TYPE DE VENTE par salle
+-- insert into VENTE (id_vente, prix_depart_vente, id_salle, annulation_vente) values (4, 1000000, 1);
+-- insert into VENTE (id_vente, prix_depart_vente, id_salle, duree_vente, date_fin) values (5, 50, 2, 'limitee', timestamp '2018-11-26 10:20:00');
+
+insert into ENCHERE values (1, 60, timestamp '2018-11-26 10:20:00', 5, 1);
+-- insert into ENCHERE values (2, 65, timestamp '2018-11-26 08:00:00', 5, 2);
+-- insert into ENCHERE values (3, 1500000, timestamp '2018-11-26 10:20:00', 1, 3);
+-- insert into ENCHERE values (4, 1500000, timestamp '2018-11-26 08:00:00', 1, 4);
+-- insert into ENCHERE values (5, 55, timestamp '2018-11-26 06:00:00', 5, 5);
 
 insert into RENTRE_DANS values ('paul.dupont@gmail.com', 1);
 insert into RENTRE_DANS values ('pierre.durand@gmail.com', 2);
 insert into RENTRE_DANS values ('pierre.durand@gmail.com', 3);
 
 insert into PRODUIT_SOUMIS_A_LA_VENTE values (1, 3, 'paul.dupont@gmail.com');
-insert into PRODUIT_SOUMIS_A_LA_VENTE values (2, 4, 'paul.dupont@gmail.com');
+-- insert into PRODUIT_SOUMIS_A_LA_VENTE values (2, 4, 'paul.dupont@gmail.com');
 insert into PRODUIT_SOUMIS_A_LA_VENTE values (3, 1, 'pierre.durand@gmail.com');
-insert into PRODUIT_SOUMIS_A_LA_VENTE values (4, 5, 'pierre.durand@gmail.com');
+-- insert into PRODUIT_SOUMIS_A_LA_VENTE values (4, 5, 'pierre.durand@gmail.com');
 insert into PRODUIT_SOUMIS_A_LA_VENTE values (5, 2, 'pierre.durand@gmail.com');
 
 insert into ENCHERE_PROPOSEE values (1, 1, 'paul.dupont@gmail.com');
-insert into ENCHERE_PROPOSEE values (2, 2, 'paul.dupont@gmail.com');
-insert into ENCHERE_PROPOSEE values (3, 3, 'pierre.durand@gmail.com');
-insert into ENCHERE_PROPOSEE values (4, 4, 'pierre.durand@gmail.com');
-insert into ENCHERE_PROPOSEE values (5, 5, 'paul.dupont@gmail.com');
+-- insert into ENCHERE_PROPOSEE values (2, 2, 'paul.dupont@gmail.com');
+-- insert into ENCHERE_PROPOSEE values (3, 3, 'pierre.durand@gmail.com');
+-- insert into ENCHERE_PROPOSEE values (4, 4, 'pierre.durand@gmail.com');
+-- insert into ENCHERE_PROPOSEE values (5, 5, 'paul.dupont@gmail.com');
 
 insert into SALLE_DE_VENTE_CREEE values (1, 1, 3);
-insert into SALLE_DE_VENTE_CREEE values (1, 2, 4);
+-- insert into SALLE_DE_VENTE_CREEE values (1, 2, 4);
 insert into SALLE_DE_VENTE_CREEE values (2, 3, 1);
-insert into SALLE_DE_VENTE_CREEE values (2, 4, 5);
+-- insert into SALLE_DE_VENTE_CREEE values (2, 4, 5);
 insert into SALLE_DE_VENTE_CREEE values (3, 5, 2);
 
 end;
